55d7e49039ba3d355dac0c81651224f002eea40e
Allyn Alda <Teddy@om.home>
2019-09-24 11:19:18 +0200

    fetch request

--- src/App.js
+++ src/App.js
@@ -2,9 +2,39 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const uri = 'https://api.twitch.tv/kraken/streams';
+
+  let h = new Headers();
+  h.append('Client-ID', 'e58bb9u3nmtqp4ikxaz1qg05kelpy7l');
+  h.append('Accept', 'application/vnd.twitchtv.v5+json');
+
+  let req = new Request(uri, {
+    method: 'GET',
+    headers: h,
+    mode: 'cors'
+  });
+
+  fetch(req)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('BAD HTTP');
+      }
+    })
+    .then((jsonData) => {
+      console.log(jsonData);
+    })
+    .catch((err) => {
+      console.log('ERROR: ', err.message);
+    })
+
+
+
   return (
     <div className="App">
-
+        <h1>Stream Hatchet</h1>
     </div>
   );
 }
